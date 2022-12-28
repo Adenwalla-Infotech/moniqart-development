7ab58ddb31d15d8928177a28605eed2ca4148fc0
deveRana <developerrana0509@gmail.com>
2022-12-28 23:32:17 +0530

    donw with about us

--- assets/frontend/js/main.js
+++ assets/frontend/js/main.js
@@ -9,3 +9,29 @@ mentBtn.addEventListener("click" , ()=>{
     mobileMenu.classList.toggle('active');
 
 })
+
+
+// Faqs
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq)=>{
+    
+    
+
+    const header = faq.querySelector(".question__answer .faq_heading");
+    
+    const paragraph = faq.querySelector(".question__answer .faq_desc");
+
+    header.addEventListener('click',()=>{
+        faqs.forEach((newFaq)=>{
+
+            if(faq !== newFaq){
+                newFaq.classList.remove("active")
+            }
+        })
+
+        faq.classList.toggle("active")
+
+    })
+
+})
