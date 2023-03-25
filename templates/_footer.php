bb2992999ab16f030de07397b1bba7f9d7b30c91
deveRana <developerrana0509@gmail.com>
2023-03-26 00:58:52 +0530

    done

--- templates/_footer.php
+++ templates/_footer.php
@@ -3,30 +3,28 @@
 
     <div class="container ">
 
-        <div class="footer__top row">
+        <div class="footer__top row px-lg-0 px-sm-2 pt-5">
 
 
-            <div
-                class="footer__list col-lg-3 col-md-4 col-sm-12 d-flex flex-column align-items-start justify-content-between ">
+            <div class="footer__list  col-lg-3 col-md-4 col-sm-12 d-flex flex-column align-items-start justify-content-between ">
                 <a href="#">
                     <img src="<?php echo base_url('uploads/images/' . _siteconfig('_sitelogo')); ?>" alt="">
                 </a>
-                <p>
+                <p class="pt-lg-0 pt-3" >
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. In at officia corporis nesciunt placeat
                 </p>
 
 
-                <span> <i class="fa-solid fa-envelope-open"></i>
+                <span class="pt-lg-0 pt-3" > <i class="fa-solid fa-envelope-open"></i>
                     <?php echo _siteconfig('_siteemail'); ?>
                 </span>
-                <span> <i class="fa-solid fa-phone"></i>&nbsp;
+                <span class="pt-lg-0 pt-3" > <i class="fa-solid fa-phone"></i>
                     <?php echo _siteconfig('_sitephone'); ?>
                 </span>
-                <span> <i class="fa-solid fa-location-dot"></i> Address </span>
+                <span class="pt-lg-0 pt-3" > <i class="fa-solid fa-location-dot"></i> Address </span>
             </div>
 
-            <div
-                class="footer__list col-lg-2 col-md-4 col-sm-12 my-3 d-flex flex-column align-items-start justify-content-start">
+            <div class="footer__list pt-5 col-lg-2 col-md-4 col-sm-12 d-flex flex-column align-items-start justify-content-start">
                 <h2>Quick Links</h2>
 
                 <ul>
@@ -37,8 +35,7 @@
 
             </div>
 
-            <div
-                class="footer__list col-lg-2 col-md-4 col-sm-12 my-3 d-flex flex-column align-items-start justify-content-start">
+            <div class="footer__list pt-5 col-lg-2 col-md-4 col-sm-12 d-flex flex-column align-items-start justify-content-start">
                 <h2>Important Links</h2>
                 <ul>
                     <li><a href="#">About</a></li>
@@ -51,20 +48,15 @@
                 </ul>
             </div>
 
-            <div
-                class="footer__list col-lg-2 col-md-4 col-sm-12 my-3 d-flex flex-column align-items-start justify-content-start">
+            <div class="footer__list pt-5 col-lg-2 col-md-6 col-sm-12 d-flex flex-column align-items-start justify-content-start">
                 <h2>Maps</h2>
-                <div style="width: 100%"><iframe width="100%" height="200" frameborder="0" scrolling="no"
-                        marginheight="0" marginwidth="0" style="border-radius: 10px;"
-                        src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a
-                            href="https://www.maps.ie/distance-area-calculator.html">area maps</a></iframe></div>
+                <div style="width: 100%"><iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style="border-radius: 10px;" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">area maps</a></iframe></div>
             </div>
 
-            <div
-                class="footer__list col-lg-3 col-md-4 col-sm-12 my-3 d-flex flex-column align-items-start justify-content-start">
+            <div class="footer__list pt-5 col-lg-3 col-md-6 col-sm-12 d-flex flex-column align-items-start justify-content-start">
                 <h2>Contact Us</h2>
 
-                <div class="form d-flex flex-column align-items-start justify-content-start">
+                <div class="form w-100 d-flex flex-column align-items-start justify-content-start">
                     <input type="text" placeholder="Your Name*">
                     <input type="text" placeholder="Your Email Address*">
                     <textarea name="comments" id="comments" cols="30" rows="4" placeholder="comments*"></textarea>
@@ -78,22 +70,34 @@
 
         </div>
 
-        <div class="footer__bottom">
-            <p>
-                Follow us
-            </p>
-            <a href="#">
-                <i class="fa-brands fa-facebook"></i>
-            </a>
-            <a href="#">
-                <i class="fa-brands fa-instagram"></i>
-            </a>
-            <a href="#">
-                <i class="fa-brands fa-twitter"></i>
-            </a>
-            <a href="#">
-                <i class="fa-brands fa-snapchat"></i>
-            </a>
+        <div class="footer__bottom row px-lg-0 px-sm-2 py-4">
+
+            <div class="  col-lg-3 col-md-4 col-sm-12 d-flex flex-column align-items-start justify-content-between ">
+            </div>
+            <div class="  col-lg-2 col-md-4 col-sm-12 d-flex flex-column align-items-start justify-content-start">
+            </div>
+            <div class="  col-lg-2 col-md-4 col-sm-12 d-flex flex-column align-items-start justify-content-start">
+            </div>
+            <div class="  col-lg-2 col-md-6 col-sm-12 d-flex flex-column align-items-start justify-content-start">
+            </div>
+
+            <div class=" col-lg-3 col-md-6 col-sm-12 d-flex flex-row align-items-center justify-content-between ">
+                <p>
+                    Follow us
+                </p>
+                <a href="#">
+                    <i class="fa-brands fa-facebook"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-brands fa-snapchat"></i>
+                </a>
+            </div>
         </div>
     </div>
 
